f95afbdc063489005c72b78fe0aaf11e7b2a8e4a
infa-ashokeen <109689486+infa-ashokeen@users.noreply.github.com>
2023-09-29 00:04:12 -0700

    minor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
         <h1 className=' head_text orange_gradient '>by ThatIITGirl</h1>
       <h2 className='desc'>
         Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries
+        that transforms lengthy articles into clear and concise summary
       </h2>
     </header>
     
